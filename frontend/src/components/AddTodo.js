91872642fd37635ce3a02bdd1e85cd650b888292
Adam Kay <adam.kay@fdmgroup.com>
2023-12-16 10:36:47 +0000

    add to-do form input working (name only)

--- frontend/src/components/AddTodo.js
+++ frontend/src/components/AddTodo.js
@@ -1,12 +1,46 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 import AddTodoIconBar from "./AddTodoIconBar";
 
 const AddTodo = () => {
+    const [taskName, setTaskName] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post('http://localhost:3000/api/v1/tasks', {
+                name: taskName,
+            });
+
+            if (response.status === 200) {
+                console.log('Task added successfully!');
+                // TODO: show the newly added task
+
+                setTaskName("");
+            } else {
+                console.error('Failed to add task.');
+            }
+        } catch (error) {
+            console.error('An error occurred:', error);
+        }
+    };
+
+    const handleInputChange = (e) => {
+        setTaskName(e.target.value);
+    };
+
     return (
         <div className="add-todo">
             <h2>Add a To-Do...</h2>
             <div className="form-container">
-                <form>
-                    <input type="text" placeholder="What do you need to-do?" />
+                <form onSubmit={handleSubmit} id="add-todo-form">
+                    <input 
+                        type="text" 
+                        placeholder="What do you need to-do?"
+                        value={taskName}
+                        onChange={handleInputChange} 
+                    />
                 </form>
                 <div className="icon-bar-container">
                     <AddTodoIconBar />
