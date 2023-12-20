111783572828362d53fc7d4c37e1fbc0905d4472
Rana Salem <ranatasalem@gmail.com>
2023-12-20 13:02:19 +0000

    Tests in TaskService for GET & POST requests are passing.

--- backend/test/controller/taskController.test.js
+++ backend/test/controller/taskController.test.js
@@ -1,103 +1,115 @@
-const request = require('supertest');
-const express = require('express');
-const TaskController = require('../../controller/TaskController');
-const TaskService = require('../../service/TaskService');
+const request = require("supertest");
+const express = require("express");
+const TaskController = require("../../controller/TaskController");
+const TaskService = require("../../service/TaskService");
 
-jest.mock('../../service/TaskService');
+jest.mock("../../service/TaskService");
 
 const app = express();
 app.use(express.json());
-app.use('api/v1/tasks', TaskController);
+app.use("api/v1/tasks", TaskController);
 
-describe('TaskController', () => {
+describe("TaskController", () => {
     afterEach(() => {
         jest.clearAllMocks();
     });
 
-    it('should get all tasks', async () => {
-        const mockedTasks = [{ id: 1, title: 'Task 1' }, { id: 2, title: 'Task 2' }];
-        TaskService.getAllTasks.mockResolvedValue(mockedTasks);
+    it("should get all tasks", async () => {
+        const mockTasks = [
+            {
+                id: 1,
+                name: "Mock Task 1",
+                description: "Description for Mock Task 1",
+                endDate: "2023-12-20",
+                completed: true,
+            },
+            {
+                id: 2,
+                name: "Mock Task 2",
+                description: "Description for Mock Task 2",
+                endDate: "2024-02-14",
+                completed: false,
+            },
+        ];
+        TaskService.getAllTasks.mockResolvedValue(mockTasks);
 
-        const response = await request(app).get('/tasks');
+        const response = await request(app).get("/api/v1/tasks");
         expect(response.status).toBe(200);
-        expect(response.body).toEqual(mockedTasks);
+        expect(response.body).toEqual(mockTasks);
     });
 
-    it('should get a task by id', async () => {
-        const mockedTask = { id: 1, title: 'Task 1' };
-        TaskService.getTaskById.mockResolvedValue(mockedTask);
+    it("should get a task by id", async () => {
+        const id = 1;
+        const mockTask = {
+            id: id,
+            name: "Mock Task 1",
+            description: "Description for Mock Task 1",
+            endDate: "2025-05-07",
+            completed: true,
+        };
+        TaskService.getTaskById.mockResolvedValue(mockTask);
 
-        const response = await request(app).get('/tasks/1');
+        const response = await request(app).get(`/api/v1/tasks/${id}`);
         expect(response.status).toBe(200);
-        expect(response.body).toEqual(mockedTask);
+        expect(response.body).toEqual(mockTask);
     });
 
-    it('should create a new task', async () => {
-        const newTaskData = { title: 'New Task', description: 'A newly created task' };
-        TaskService.createTask.mockResolvedValue(newTaskData);
+    it("should create a new task", async () => {
+        const newMockTask = {
+            name: "New Mock Task",
+            description: "Description for New Mock Task",
+            endDate: "2050-07-12",
+            completed: false,
+        };
+        TaskService.createTask.mockResolvedValue(newMockTask);
 
-        const response = await request(app)
-            .post('/tasks')
-            .send(newTaskData);
+        const response = await request(app).post("/api/v1/tasks").send(newMockTask);
 
         expect(response.status).toBe(201);
-        expect(response.body).toEqual(newTaskData);
+        expect(response.body).toEqual(newMockTask);
     });
 
-    it('should update a task by id', async () => {
-        const taskId = 1;
-        const updatedTaskData = { title: 'Updated Task', description: 'An updated task' };
-        TaskService.updateTask.mockResolvedValue(updatedTaskData);
+    it("should update a task by id", async () => {
+        const id = 1;
+        const updatedMockTask = {
+            name: "Updated Mock Task",
+            description: "Description for Updated Mock Task",
+            endDate: "2075-04-02",
+            completed: true,
+        };
+        TaskService.updateTask.mockResolvedValue(updatedMockTask);
 
         const response = await request(app)
-            .put(`/tasks/${taskId}`)
-            .send(updatedTaskData);
+            .put(`/api/v1/tasks/${id}`)
+            .send(updatedMockTask);
 
         expect(response.status).toBe(200);
-        expect(response.body).toEqual(updatedTaskData);
+        expect(response.body).toEqual(updatedMockTask);
     });
 
-    it('should patch a task by id', async () => {
-        const taskId = 1;
-        const patchedTaskData = { description: 'Patched task description' };
-        TaskService.patchTask.mockResolvedValue(patchedTaskData);
+    it("should patch a task by id", async () => {
+        const id = 1;
+        const patchedMockTaskData = {
+            description: "Description for Patched Mock Task",
+        };
+        TaskService.patchTask.mockResolvedValue(patchedMockTaskData);
 
         const response = await request(app)
-            .patch(`/tasks/${taskId}`)
-            .send(patchedTaskData);
+            .patch(`/api/v1/tasks/${id}`)
+            .send(patchedMockTaskData);
 
         expect(response.status).toBe(200);
-        expect(response.body).toEqual(patchedTaskData);
+        expect(response.body).toEqual(patchedMockTaskData);
     });
 
     it('should delete a task by id', async () => {
-        const taskId = 1;
-        const deleteMessage = { message: 'Task successfully deleted.' };
+        const id = 1;
+        const deleteMessage = { message: "Task successfully deleted." };
         TaskService.deleteTaskById.mockResolvedValue(deleteMessage);
 
-        const response = await request(app).delete(`/tasks/${taskId}`);
+        const response = await request(app).delete(`/api/v1/tasks/${id}`);
 
         expect(response.status).toBe(200);
         expect(response.body).toEqual(deleteMessage);
     });
-
-    it('should return 404 if task not found (GET by id)', async () => {
-        TaskService.getTaskById.mockResolvedValue(null);
-
-        const response = await request(app).get('/tasks/999');
-
-        expect(response.status).toBe(404);
-        expect(response.body).toEqual({ message: 'Task not found.' });
-    });
-
-    it('should return 404 if task not found (DELETE by id)', async () => {
-        TaskService.deleteTaskById.mockResolvedValue(null);
-
-        const response = await request(app).delete('/tasks/999');
-
-        expect(response.status).toBe(404);
-        expect(response.body).toEqual({ message: 'Task not found.' });
-    });
-
-    // Add other test cases as necessary
 });
--- backend/test/service/taskService.test.js
+++ backend/test/service/taskService.test.js
@@ -1,29 +1,84 @@
-const { Task } = require('../../database/associations');
-const TaskService = require('./TaskService');
+const { Task } = require("../../database/associations");
+const TaskService = require("../../service/TaskService");
 
-jest.mock('../../database/associations');
+jest.mock("../../database/associations");
 
-describe('TaskService', () => {
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+describe("TaskService", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-    it('should get all tasks', async () => {
-        const mockedTasks = [{ id: 1, title: 'Task 1' }, { id: 2, title: 'Task 2' }];
-        Task.findAll.mockResolvedValue(mockedTasks);
+  // Test: GET all tasks
+  it("should get all tasks", async () => {
+    const mockTasks = [
+      {
+        id: 1,
+        name: "Mock Task 1",
+        description: "Description for Mock Task 1",
+        endDate: "2023-12-25",
+        completed: false,
+      },
+      {
+        id: 2,
+        name: "Mock Task 2",
+        description: "Description for Mock Task 2",
+        endDate: "2050-01-13",
+        completed: true,
+      },
+    ];
+    Task.findAll.mockResolvedValue(mockTasks);
 
-        const tasks = await TaskService.getAllTasks();
-        expect(tasks).toEqual(mockedTasks);
-    });
+    const tasks = await TaskService.getAllTasks();
+    expect(tasks).toEqual(mockTasks);
+  });
 
-    it('should get a task by id', async () => {
-        const taskId = 1;
-        const mockedTask = { id: taskId, title: 'Task 1' };
-        Task.findByPk.mockResolvedValue(mockedTask);
+  // Test: GET task by id
+  it("should get a task by id", async () => {
+    const id = 1;
+    const mockTask = {
+      id: id,
+      name: "Mock Task 1",
+      description: "Description for Mock Task 1",
+      endDate: "2098-03-25",
+      completed: false,
+    };
+    Task.findByPk.mockResolvedValue(mockTask);
 
-        const task = await TaskService.getTaskById(taskId);
-        expect(task).toEqual(mockedTask);
-    });
+    const task = await TaskService.getTaskById(id);
+    expect(task).toEqual(mockTask);
+  });
 
-    // Add other test cases for createTask, updateTask, patchTask, deleteTaskById similarly
+  // Test: POST create task
+  it("should create a task", async () => {
+    const mockTaskData = {
+      name: "New Mock Task",
+      description: "Description for New Mock Task",
+      endDate: "2034-09-24",
+      completed: true,
+    };
+    const mockCreatedTask = { id: 1, ...mockTaskData };
+    Task.create.mockResolvedValue(mockCreatedTask);
+
+    const newMockTask = await TaskService.createTask(mockTaskData);
+    expect(newMockTask).toEqual(mockCreatedTask);
+  });
+
+  // Test: PUT update task
+  it("should update a task", async () => {
+    const id = 1;
+    const mockTaskData = {
+      name: "Mock Updated Task",
+      description: "Description for Mock Updated Task",
+      endDate: "2068-07-22",
+      completed: true,
+    };
+    const mockTask = { id: id, ...mockTaskData };
+    Task.findByPk.mockResolvedValue(mockTask);
+    Task.update.mockResolvedValue(mockTask);
+
+    const mockUpdatedTask = await TaskService.updateTask(id, mockTaskData);
+    expect(mockUpdatedTask).toEqual(mockTask);
+  });
+
+  it("");
 });
